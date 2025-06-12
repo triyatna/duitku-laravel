735eb3ef1a7aa9bb4c88981e054a9bbdfef878d0
Triyatna <triyatna990@gmail.com>
2025-06-13 01:24:55 +0700

    update struktur

---
+++ src/Facades/Duitku.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Triyatna\DuitkuLaravel\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @method static array getPaymentMethods(int $amount)
+ * @method static array createInvoice(int $paymentAmount, string $paymentMethod, string $merchantOrderId, string $productDetails, string $customerVaName, string $email, string|null $phoneNumber, string $callbackUrl, string $returnUrl, int $expiryPeriod = 1440, array|null $itemDetails = null, array|null $customerDetail = null)
+ * @method static array checkTransactionStatus(string $merchantOrderId)
+ * @method static \Illuminate\Http\JsonResponse handleCallback()
+ *
+ * @see \Triyatna\DuitkuLaravel\Duitku
+ */
+class Duitku extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor(): string
+    {
+        return 'duitku';
+    }
+}
